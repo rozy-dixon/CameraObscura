577fa92b7d3dfd9e00a949e4f1beea4a3f319653
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-03-08 17:30:50 -0800

    Implemented prototype main character with movement

---
+++ Game Files/Scripts/main_character.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+# Put character's movement attributes in the inspector so they can be modified
+@export var push_force  = 1000
+@export var walk_speed  = 40.0
+
+# Function that runs on physics tick, rather than every frame.
+func _physics_process(delta):
+	# Process character movement input
+	movement_input()
+	# Push a RigidBody2D node if we are colliding with one
+	push()
+	# Apply the calculated physics to our character for this tick
+	move_and_slide()
+	
+func push():
+	# If we are moving
+	if $".".move_and_slide():
+		# For each point of collision
+		for collisionIndex in $".".get_slide_collision_count():
+			# Store the collision in a variable
+			var collision = $".".get_slide_collision(collisionIndex)
+			# If we are colliding with a RigidBody2D
+			if collision.get_collider() is RigidBody2D:
+				# Apply a force equal to our push_force attribute, located at the point of collision, in the opposite direction of that point's normal vector
+				collision.get_collider().apply_force(collision.get_normal() * -push_force)
+
+func movement_input():
+	# Store the input from the arrow keys into a 2D vector, Ex: (+/-x, +/-y), (1,0) = left, (-1,0) = right, (0,1) = up, (0,-1) = down, (+/-0.70707..., +/-0.70707...) = diagonal, (0,0) = idle
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	print(direction)
+	# If we are not idle
+	if(direction):
+		# Apply velocity by multiplying the movement vector by a constant, our attribute, walk_speed
+		velocity = direction * walk_speed
+		print(direction * walk_speed)
+		# If we are moving in the x direction
+		if direction.x:
+			# Change to the horizontal sprite
+			$AnimatedSprite2D.frame = 2
+			# If we are moving left, flip the sprite horizontally
+			if direction.x < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
+		# If we are moving in the y direction
+		if direction.y:
+			# If we are moving down, use the downward facing sprite, if we are moving up, use the upward facing sprite
+			if direction.y < 0:
+				$AnimatedSprite2D.frame = 1
+			else:
+				$AnimatedSprite2D.frame = 0
+	# If we are not moving, linearly interpolate (lerp/smoothly change) our velocity to zero so the stopping feels like decelerating rather than rigidly stopping
+	else:
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
+		velocity.y = move_toward(velocity.y, 0, walk_speed)
+
